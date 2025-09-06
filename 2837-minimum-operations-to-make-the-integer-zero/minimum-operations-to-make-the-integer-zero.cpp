6cdc8372aecec8a63786e25dcf4804ad27800a4a
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-09-06 21:59:39 +0530

    Time: 1 ms (6.30%) | Memory: 8.3 MB (11.02%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        if (num2==0) return popcount(unsigned(num1));
-        long long x=num1;
-        for(int k=1; k<36; k++){
-            x-=num2;
-            if (x<k) return -1;
-            if (x>=0 && k>=popcount((unsigned long long)(x))) return k;
+        for (int t = 1; t <= 60; t++) {
+            long long s = (long long)num1 - (long long)t * num2;
+            if (s < 0) continue;
+            if (s < t) continue;
+            int ones = __builtin_popcountll(s);
+            if (ones <= t) return t;
         }
         return -1;
     }
