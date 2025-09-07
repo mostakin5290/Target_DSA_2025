9541b6936f414c18621579a1b1955df5b5d490c4
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-09-08 01:29:42 +0530

    Time: 253 ms (68.62%) | Memory: 133.5 MB (69.78%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int r = grid.size();
-        int c = grid[0].size();
-        int c_start = c;
-        int c_end = 0;
-        int r_start = r;
-        int r_end = 0;
+        int row = grid.size();
+        int col = grid[0].size();
 
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        int rowStart = row;
+        int rowEnd = 0;
+
+        int colStart = col;
+        int colEnd = 0;
+
+        for(int i=0; i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]==1){
-                    c_start = min(c_start,j);
-                    c_end = max(c_end,j);
-                    r_start = min(r_start,i);
-                    r_end = max(r_end,i);
+                    colStart = min(colStart ,j);
+                    colEnd = max(colEnd,j);
+
+                    rowStart = min(rowStart , i);
+                    rowEnd = max(rowEnd,i);
                 }
-                
             }
         }
-        int c_ans = (c_end - c_start) + 1;
-        int r_ans = (r_end - r_start) + 1;
 
-        return c_ans*r_ans;
+        int colAns = (colEnd - colStart)+1;
+        int rowAns = (rowEnd - rowStart)+1;
+
+        return colAns * rowAns;
     }
 };
