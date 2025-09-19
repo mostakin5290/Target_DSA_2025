fc7d48955b5460ad93b2b458c7f475dc5cc9b7a6
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-09-19 11:22:47 +0530

    Time: 121 ms (41.22%) | Memory: 122.5 MB (89.89%) - LeetSync

---
+++ 3797-design-spreadsheet/design-spreadsheet.cpp
@@ -0,0 +1,41 @@
+class Spreadsheet {
+public:
+    map <string,int> un;
+    Spreadsheet(int rows) {
+        
+    }
+    
+    void setCell(string cell, int value) {
+        un[cell]=value;
+    }
+    
+    void resetCell(string cell) {
+        un[cell]=0;
+    }
+
+    int getCellValue(string token){
+        if(un.find(token)!=un.end())
+            return un[token];
+            
+        if(isalpha(token[0]))
+            return 0;
+        return stoi(token);
+    }
+    
+    int getValue(string formula) {
+        formula = formula.substr(1);
+        int plus = formula.find('+');
+        string left = formula.substr(0,plus);
+        string right = formula.substr(plus+1);
+
+        return getCellValue(left) + getCellValue(right);
+    }
+};
+
+/**
+ * Your Spreadsheet object will be instantiated and called as such:
+ * Spreadsheet* obj = new Spreadsheet(rows);
+ * obj->setCell(cell,value);
+ * obj->resetCell(cell);
+ * int param_3 = obj->getValue(formula);
+ */
