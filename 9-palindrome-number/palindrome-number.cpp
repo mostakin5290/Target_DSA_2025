08f32a62a2277c592393a29e1c7bd55c58d2b6a1
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-11-06 11:30:26 +0530

    Time: 1 ms (59.18%) | Memory: 8.6 MB (68.32%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0)return false;
+        int dup_x = x;
+        int rev = 0;
 
-        string str_x = to_string(x);
-        int start=0;
-        int end = str_x.length()-1;
-
-        while(start<end){
-            if(str_x[start]==str_x[end]){
-                start++;
-                end--;
-            }
-            else
-                return false;
+        while(x>0){
+            int temp = x%10;
+            if(rev>INT_MAX/10 || rev<INT_MIN/10)return false;
+            rev = (rev*10) + temp;
+            x = x/10;
         }
-        return true;
+
+        return dup_x == rev;
     }
 };
