79eeaec745c9deafe569bf28a8bea29ea12d5e19
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-11-07 10:29:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> f(n+3);
-        f[0]=0; f[1]=1;
-        for(int i=2;i<=n;++i) 
-            f[i]=f[i-1]+f[i-2];
-        return f[n];
+       if(n==0)return 0;
+       if(n==1)return 1;
+       int ans = 1;
+       int prev = 0;
+
+       for(int i = 1 ; i < n ; i++) {
+        int temp = ans;
+        ans+=prev;
+        prev=temp;
+       }
+
+       return ans;
     }
 };
