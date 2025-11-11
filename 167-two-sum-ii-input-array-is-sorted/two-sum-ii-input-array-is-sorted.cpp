4246247a988be4c44374b46e4fb2cc77e0d7a4a4
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-11-11 13:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (93.36%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
-        // cout<<n;
         // vector <int> ans(2);
         int j=n-1;
         for(int i = 0; i < j;){
@@ -15,14 +14,9 @@ public:
             //     } 
             // }
             int sum = numbers[i]+numbers[j];
-
             if(sum==target) return {i+1,j+1};
-
             else if(sum > target) j--;
-
             else i++;
-            
-
         }
         return {};
     }
