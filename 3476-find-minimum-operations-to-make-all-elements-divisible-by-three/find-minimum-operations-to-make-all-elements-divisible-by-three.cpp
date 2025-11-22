c748220fa6bcf18288a3de6f86e03a6811a2a8c6
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2025-11-23 00:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (79.62%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -3,11 +3,9 @@ public:
     int minimumOperations(vector<int>& nums) {
         int operations = 0;
         
-        for (int x : nums) {
-            if (x % 3 != 0) {
+        for (int x : nums)
+            if (x % 3 != 0)
                 operations++;
-            }
-        }
         
         return operations;
     }
