36aebd36f75b71d642ff05dbb32052cfefddfaa9
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-01-01 21:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 29.4 MB (86.71%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    void Rotate_temp(vector<int>& arr,int i , int j){
+        while(i<j){
+            int temp = arr[i];
+            arr[i]=arr[j];
+            arr[j]= temp;
+            i++;
+            j--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int>ans(n);
+        k=k%n;
+        if(!k)return;
 
-        for(int i = 0; i<n; i++){
-            ans[(k+i)%n] = nums[i];
-        }
-
-        for(int i = 0; i<n; i++){
-            nums[i] = ans[i];
-        }
+        Rotate_temp(nums,0,n-1);
+        Rotate_temp(nums,0,k-1);
+        Rotate_temp(nums,k,n-1);
     }
 };
