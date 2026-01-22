a0c5b2566f22713bf786933d28065a6b155e5b21
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-01-22 21:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.29%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0,j=0;
+        while(i!=n){
+            if(nums[i]==0) i++;
+            else if(nums[i]!=0 && nums[j]==0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+                i++;
+            }
+            else{i++;j++;} 
+            
+
+        }
+    }
+};
