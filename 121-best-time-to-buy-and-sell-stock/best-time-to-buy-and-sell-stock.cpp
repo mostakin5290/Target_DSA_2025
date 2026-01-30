c179ca1860db93549857b1cf3d758dbb4ff50de4
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-01-30 19:08:32 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (24.19%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int minPriceSoFar = prices[0];
         int maxProfit = 0;
 
-        for(int i=0;i<prices.size();i++){
+        for(int i=1;i<prices.size();i++){
             int Profit = prices[i] - minPriceSoFar; 
             if(Profit>maxProfit) maxProfit = Profit;
             if(minPriceSoFar>prices[i])minPriceSoFar=prices[i];
