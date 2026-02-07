f674bd31d2602ddd2341fcda8d326ff2ef59e6fc
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-02-07 13:05:52 +0530

    Time: 47 ms (24.11%) | Memory: 25.5 MB (72.34%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int minimumDeletions(string s){
-
-        int n = s.length();
-        stack<char> st;
-        int count = 0;
-
-        for(char ch: s){
-
-            if(st.empty())
-                st.push(ch);
-            else if((st.top() == ch) || ((st.top() == 'a') && (ch == 'b')))
-                st.push(ch);
-            else {
-                st.pop();
-                count++;
-            }
-        }
+    int minimumDeletions(string s) {
         
+        int n = s.length();
+        int total_count_A = 0;
+        int left_count_B = 0;
+        int count = INT_MAX;
+
+        for(char ch: s) {
+            if(ch == 'a')
+               total_count_A++; 
+        }
+
+        for(char ch: s) {
+
+            if(ch == 'a')
+                total_count_A--;
+            
+            int del = (total_count_A + left_count_B);
+            count = min(count, del);
+
+            if(ch == 'b')
+               left_count_B++; 
+        }
+
         return count;
     }
 };
