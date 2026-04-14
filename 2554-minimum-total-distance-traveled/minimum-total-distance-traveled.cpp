dfdae9c16933c3232a971fcf9299dc8ba58f56f6
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-04-14 08:38:53 +0530

    Time: 19 ms (72.60%) | Memory: 13.4 MB (77.40%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -8,20 +8,20 @@ public:
         int m = factory.size();
 
         const long long INF = 1e18;
-        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, INF));
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1, INF));
 
-        for (int j = 0; j <= m; j++)
-            dp[0][j] = 0;
+        for(int j = 0; j <= m; j++) dp[0][j] = 0;
 
-        for (int j = 1; j <= m; j++) {
+        for(int j = 1; j <= m; j++) {
             int pos = factory[j-1][0];
             int limit = factory[j-1][1];
 
-            for (int i = 0; i <= n; i++) {
-                dp[i][j] = dp[i][j-1]; // skip factory
+            for(int i = 0; i <= n; i++) {
+                dp[i][j] = dp[i][j-1];
 
                 long long dist = 0;
-                for (int k = 1; k <= limit && i - k >= 0; k++) {
+
+                for(int k = 1; k <= limit && i-k >= 0; k++) {
                     dist += abs(robot[i-k] - pos);
                     dp[i][j] = min(dp[i][j], dp[i-k][j-1] + dist);
                 }
