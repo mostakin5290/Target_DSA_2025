38e26fbdd1d8e9186a078347b0b4005835575a8d
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-04-15 23:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (62.34%) - LeetSync

---
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int start) {
+        int n = words.length;
+
+        for(int i = 0; i <= n/2; i++) {
+            if(words[(start+i)%n].equals(target) ||
+               words[(start-i+n)%n].equals(target))
+                return i;
+        }
+
+        return -1;
+    }
+}
