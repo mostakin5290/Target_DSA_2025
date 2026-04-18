3b50c557c458ad756ab42ae2c7904c6dc72cd03e
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-04-18 06:50:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.21%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int reverseNumber(int n) {
         int revNum = 0, remender;
-        while (n != 0) {
+
+        while(n != 0) {
             remender = n % 10;
             revNum = revNum * 10 + remender;
             n /= 10;
         }
+
         return revNum;
     }
 
     int mirrorDistance(int n) {
         int revNum = reverseNumber(n);
-        return abs(n-revNum);
+        return abs(n - revNum);
     }
 };
