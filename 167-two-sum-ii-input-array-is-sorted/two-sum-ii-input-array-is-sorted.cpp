aceddfaf17e7ba47169842afa085ba08a6245844
Mostakin Mondal <128492762+mostakin5290@users.noreply.github.com>
2026-04-26 20:16:34 +0530

    Time: 3 ms (9.45%) | Memory: 19.4 MB (63.91%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int n = numbers.size();
-        // vector <int> ans(2);
-        int j=n-1;
-        for(int i = 0; i < j;){
-            // for(int j = i+1; j < n; j++){
-            //     if(numbers[i]+numbers[j] == target) {
-            //         // cout<<numbers[i]+numbers[j]<<" ";
-            //         ans[0]=(i+1);
-            //         ans[1]=(j+1);
-            //         return ans;
-            //     } 
-            // }
-            int sum = numbers[i]+numbers[j];
-            if(sum==target) return {i+1,j+1};
-            else if(sum > target) j--;
-            else i++;
+        int st = 0;
+        int end = numbers.size()-1;
+
+        while(st < end){
+            int sum = numbers[st] + numbers[end];
+            if(sum == target ) return {st+1,end+1};
+            else if (sum < target) st++;
+            else end--;
         }
-        return {};
+        return {-1,-1};
     }
 };
